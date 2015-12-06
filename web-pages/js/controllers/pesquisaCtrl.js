bbe6f831f711c821de44ae475a34368cfaad7c9c
Rerisson Daniel <rerissondaniel@gmail.com>
2015-12-06 09:23:48 -0300

    Initial commit

---
+++ web-pages/js/controllers/pesquisaCtrl.js
@@ -0,0 +1,56 @@
+angular.module("pesquisaSimpif").controller("pesquisaCtrl", function($scope, $http, $filter){
+		$scope.app = "Entrega de Kits SIMPIF";
+		$scope.participants = [];
+		$scope.receivedTxt = "Recebido";
+		$scope.receivedBool = false;
+
+		$scope.changeReceived = function(){
+			$scope.receivedBool = !$scope.receivedBool;
+			if($scope.receivedBool === true){
+				$scope.receivedTxt = "Recebido";
+			}else{
+				$scope.receivedTxt = "Recebido";
+			}
+			loadparticipants();
+		}
+
+		var loadparticipants = function(searchCriteria){
+
+			$http.get("http://200.129.68.181:8773/pesquisa-simpif-service/services/get-all").success(function(data){
+				$scope.participants = data;
+
+				$scope.totalReceived = $scope.participants.filter(function(element){
+						return element.isDelivered;
+					}).length;
+				$scope.totalNotReceived = $scope.participants.filter(function(element){
+						return !element.isDelivered;
+					}).length;
+			}).error(function(data, status){
+				$scope.message = "Aconteceu um problema: " + data;
+			});
+		};
+
+		$scope.updateParticipant = function(participant){
+			confirmation = window.confirm("Confirma a entrega do kit ao participante " + participant.fullName + " ?");
+
+			if(confirmation === true){
+				$http.post("http://200.129.68.181:8773/pesquisa-simpif-service/services/deliver-kit", angular.copy(participant)).success(function(){
+					delete $scope.participant;
+					loadparticipants();
+				}).error(function(){
+					participant.isDelivered = false;
+					window.alert("Ocorreu um erro na comunicação com o servidor, favor chamar o suporte.");
+				});
+			}else{
+				participant.isDelivered = false;
+			}
+			$scope.searchCriteria = "";
+			loadparticipants();
+		}
+
+		$scope.setOrderCriteria = function(campo){
+			$scope.orderCriteria = campo;
+			$scope.orderDirection = !$scope.orderDirection;
+		};
+		loadparticipants();
+	});
