93a900def575b6479802e15b4806686a8cf7d4aa
rhavymaia <rmg3@cin.ufpe.br>
2015-12-06 15:19:34 -0300

    - Consulta de participante pelo nome.

--- pesquisa-simpif-service/src/br/com/simpif/service/Services.java
+++ pesquisa-simpif-service/src/br/com/simpif/service/Services.java
@@ -10,6 +10,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.HibernateException;
 
 import br.com.simpif.database.UserDAO;
@@ -19,11 +21,15 @@ import br.com.simpif.entities.User;
 @Path("/services")
 public class Services {
 
+	private static Logger logger = LogManager.getLogger(Services.class);
+	
 	@POST
 	@Path("/deliver-kit")
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response deliverKit(User user) {
+		
+		logger.info("Entregando o kit:" + user.getFullName());
 		
 		UserDAO.getInstance().update(user);
 
@@ -48,6 +54,8 @@ public class Services {
 	@Produces("application/json")
 	public Response insertUser(User user) {
 		
+		logger.info("Inserindo usuário:" + user.getFullName());
+		
 		ResponseBuilder builder;
 		
 		try {
@@ -64,26 +72,17 @@ public class Services {
 
 		return builder.build();
 	}
+	
+	@POST
+	@Path("/get-byname")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public List<User> findUserByName(User user){
+		
+		String fullName = user.getFullName().trim().toUpperCase();
+		
+		List<User> users = UserDAO.getInstance().getByName(fullName);
 
-	// @GET
-	// @Path("/get-delivered")
-	// @Produces("application/json")
-	// public List<User> getDelivered(){
-	//
-	// UserDAO userDao = new UserDAO();
-	// List<User> list = userDao.getDelivered();
-	//
-	// return list;
-	// }
-	//
-	// @GET
-	// @Path("/get-not-delivered")
-	// @Produces("application/json")
-	// public List<User> getNotDelivered(){
-	//
-	// UserDAO userDao = new UserDAO();
-	// List<User> list = userDao.getNotDelivered();
-	//
-	// return list;
-	// }
+		return users;		
+	}
 }
