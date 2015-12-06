61d3f67a50fde0036ce5f4d983336e6ba29676fd
rhavymaia <rmg3@cin.ufpe.br>
2015-12-06 14:07:03 -0300

    Simpif Kit
    Cliente web

---
+++ pesquisa-simpif-web/WebContent/js/controllers/pesquisaCtrl.js
@@ -0,0 +1,57 @@
+angular.module("pesquisaSimpif").controller("pesquisaCtrl", function($scope, $http, $filter){
+		$scope.app = "Entrega de Kits SIMPIF";
+		$scope.participants = [];
+		$scope.receivedTxt = "Recebido";
+		$scope.receivedBool = false;
+
+		$scope.changeReceived = function(){
+			$scope.receivedBool = !$scope.receivedBool;
+			/*
+			if($scope.receivedBool === true){
+				$scope.receivedTxt = "Recebido";
+			}else{
+				$scope.receivedTxt = "Recebido";
+			}*/
+			loadparticipants();
+		}
+
+		var loadparticipants = function(){
+
+			$http.get("http://www.ladoss.com.br:8773/pesquisa-simpif-service/services/get-all").success(function(data){
+				$scope.participants = data;
+
+				$scope.totalReceived = $scope.participants.filter(function(element){
+						return element.isDelivered;
+					}).length;
+				$scope.totalNotReceived = $scope.participants.filter(function(element){
+						return !element.isDelivered;
+					}).length;
+			}).error(function(data, status){
+				$scope.message = "Aconteceu um problema: " + data;
+			});
+		};
+
+		$scope.updateParticipant = function(participant){
+			confirmation = window.confirm("Confirma a entrega do kit ao participante " + participant.fullName + " ?");
+
+			if(confirmation === true){
+				$http.post("http://www.ladoss.com.br:8773/pesquisa-simpif-service/services/deliver-kit", angular.copy(participant)).success(function(){
+					delete $scope.participant;
+					loadparticipants();
+					$scope.searchCriteria = "";
+				}).error(function(){
+					participant.isDelivered = false;
+					window.alert("Ocorreu um erro na comunicação com o servidor, favor chamar o suporte.");
+				});
+			}else{
+				participant.isDelivered = false;
+			}
+			loadparticipants();
+		}
+
+		$scope.setOrderCriteria = function(campo){
+			$scope.orderCriteria = campo;
+			$scope.orderDirection = !$scope.orderDirection;
+		};
+		loadparticipants();
+	});
